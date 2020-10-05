1a07bde9e8365e186bdbc5148c6562e9cc1424ea
glezzz <alexglezk@gmail.com>
2020-10-05 11:29:45 +0200

    step1 done

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 //this line makes PHP behave in a more strict way
 declare(strict_types=1);
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 //we are going to use session variables so we need to enable sessions
 session_start();
 
@@ -35,3 +39,21 @@ $products = [
 $totalValue = 0;
 
 require 'form-view.php';
+
+$email = "";
+$street = "";
+$street_no = "";
+$city = "";
+$zipcode = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['email'])) {
+        $email = $_POST['email'];
+    }}
+
+
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo("$email is a valid email address");
+} else {
+    echo("$email is not a valid email address");
+}
